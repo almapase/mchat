752fea396e82ce8b92763093309f769e79abb50d
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-16 23:57:19 -0500

    mejoras en las relaciones de los modelos

--- app/models/mchat_test.rb
+++ app/models/mchat_test.rb
@@ -1,3 +1,4 @@
 class MchatTest < ActiveRecord::Base
   has_many :answer_groups
+  has_many :answers, through: :answer_groups
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,4 @@
 class Patient < ActiveRecord::Base
   has_many :answers
+  has_many :answer_groups, through: :answers
 end
