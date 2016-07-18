3f7c2d93cc741240787f3daa38e87432887fe0f9
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-18 17:25:13 -0500

    fix migration

--- db/migrate/20160713051417_create_answer_groups.rb
+++ db/migrate/20160713051417_create_answer_groups.rb
@@ -2,7 +2,7 @@ class CreateAnswerGroups < ActiveRecord::Migration
   def change
     create_table :answer_groups do |t|
       t.boolean :option
-      t.references :mchat_test, index: true, foreign_key: true
+      # t.references :mchat_test, index: true, foreign_key: true
       t.references :answer, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/migrate/20160717045942_remove_column_to_answer_group.rb
+++ db/migrate/20160717045942_remove_column_to_answer_group.rb
@@ -1,5 +1,5 @@
 class RemoveColumnToAnswerGroup < ActiveRecord::Migration
   def change
-    remove_reference :answer_groups, :mchat_test, index: true, foreign_key: true
+    # remove_reference :answer_groups, :mchat_test, index: true, foreign_key: true
   end
 end
